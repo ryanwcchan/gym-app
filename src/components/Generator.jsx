3f86f9f39bf6f30eb0c84b65a34379da0a406ab3
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2024-10-06 14:25:13 -0400

    Add new sections in generator component

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,4 +1,5 @@
-import { WORKOUTS } from "../utils/workouts"
+import { useState } from "react"
+import { SCHEMES, WORKOUTS } from "../utils/workouts"
 import SectionWrapper from "./SectionWrapper"
 
 function Header({ index, title, description }) {
@@ -14,6 +15,15 @@ function Header({ index, title, description }) {
 }
 
 export default function Generator() {
+  const [showModal, setShowModal] = useState(false)
+  const [workoutType, setWorkoutType] = useState('individual')
+  const [muscleGroup, setMuscleGroup] = useState([])
+  const [goals, setGoals] = useState('strength_power')
+
+  function toggleModal() {
+    setShowModal(!showModal)
+  }
+
   return (
     <SectionWrapper
         header={"generate your workout"}
@@ -22,13 +32,48 @@ export default function Generator() {
         <Header
             index={'01'}
             title={'Push yourself'}
-            description={'Select your pefered workout'}
+            description={'Choose your workout split'}
         />
         <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
             {Object.keys(WORKOUTS).map((type, index) => {
                 return (
-                    <button key={index} className="bg-slate-950 border border-blue-400 py-4 rounded-lg">
-                        <p>{type}</p>
+                    <button 
+                        key={index} 
+                        className="bg-slate-950 border border-blue-400 py-3 rounded-lg"
+                    >
+                        <p className="capitalize">{type.replaceAll('_', " ")}</p>
+                    </button>
+                )
+            })}
+        </div>
+        <Header
+            index={'02'}
+            title={'Targeting muscle groups'}
+            description={'Select the muscles you wish to train'}
+        />
+        <div className="bg-slate-950 py-3 border border-solid 
+        border-blue-400 rounded-lg flex flex-col">
+            <button onClick={toggleModal} className="relative flex items-center justify-center">
+                <p>Select muscle groups</p>
+                <i className="fa-solid fa-caret-down absolute right-3 top-1/2 -translate-y-1/2"></i>
+            </button>
+            {showModal && (
+                <div>modal</div>
+            )}
+        </div>
+        <Header
+            index={'03'}
+            title={'Identify your goal'}
+            description={'Select your pefered workout type'}
+        />
+        <div className="grid grid-cols-3 gap-4">
+            {Object.keys(SCHEMES).map((scheme, schemeIndex) => {
+                return (
+                    <button 
+                        key={schemeIndex} 
+                        className="bg-slate-950 border border-blue-400 py-3 rounded-lg"
+                    >
+                        <p className="capitalize">{scheme.replaceAll('_', " ")}</p>
                     </button>
                 )
             })}
